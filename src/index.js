ee4d36c82eaa175266cfbe47ecea36518fd19d72
Gabriela Guida <61994595+gabsguida@users.noreply.github.com>
2020-05-09 23:08:10 -0300

    criando novas funções/código mais limpo

--- src/index.js
+++ src/index.js
@@ -16,62 +16,62 @@ const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
 
 const getProductInfo = (productId, productList) => {
 	let product = productList.filter((product) => (product.id == productId));
-	return product.length != 0 ? product[0] : false;
+	return product.length ? product[0] : false;
 }
 
-const getPromotion = (categoryCount) => {
-	return promotions[categoryCount - 1];
+const getPromotion = categoryCount => promotions[categoryCount - 1];
+
+const getProductList = (ids, productsList) => 
+	productsList.filter((product) => ids.includes(product.id));
+
+
+const getCategoryList = (productList) => 
+	[...(new Set(productList.map((product) => product.category)))];
+
+const getProductPrice = (product, promotionName) => {
+	let productPromotion = product.promotions.filter((promotion) => promotion.looks.includes(promotionName));
+	return productPromotion.length == 1 ? productPromotion[0].price : getProductRegularPrice(product);	
 }
 
-const getShoppingCart = (ids, productsList) => {
-	let productList = [];
-	let categoryList = [];
-	
-	for(let i=0; i<ids.length; i++){
-		let product = getProductInfo(ids[i], productsList);
-		productList.push(product);
-		if(!categoryList.includes(product.category)){
-			categoryList.push(product.category);
-		}
-	}
-	
-	let promotion = getPromotion(categoryList.length);
-	let totalPrice = 0;
-	let totalPriceWithoutDiscount = 0;
-	
-	for(let x=0; x<productList.length; x++){
-		let productPrice = null;
-		for(let y=0; y<productList[x].promotions.length; y++){
-			if(productList[x].promotions[y].looks.includes(promotion)){
-				productPrice = productList[x].promotions[y].price;
-			}
-		}
-		totalPriceWithoutDiscount += productList[x].regularPrice;
-		totalPrice += (productPrice == null) ? productList[x].regularPrice : productPrice;
-	}
-	
-	let discountValue = totalPriceWithoutDiscount - totalPrice;
-	let discount = (discountValue*100)/totalPriceWithoutDiscount;
+const getProductRegularPrice = (product) => product.regularPrice;
 
-	let list = productList.map((product) => {
+const getProductListSummary = (productList) => 
+	productList.map((product) => {
 		return {
 			name: product.name,
 			category: product.category
 		}
 	})
 	
-	let cart = {
-		products: list,
-		promotion: promotion,
+const getShoppingCart = (ids, productsList) => {
+
+	const productList = getProductList(ids, productsList);
+	const categoryList = getCategoryList(productList);
+	const promotion = getPromotion(categoryList.length);
+	let totalPrice = 0;
+	let totalPriceWithoutDiscount = 0;
+	
+	productList.forEach((product) => {
+		const price = getProductPrice(product, promotion);
+		const regularPrice = getProductRegularPrice(product);
+		totalPriceWithoutDiscount += regularPrice;
+		totalPrice += price;
+	})
+	
+	const discountValue = totalPriceWithoutDiscount - totalPrice;
+	const discount = (discountValue*100)/totalPriceWithoutDiscount;
+	
+	const cart = {
+		products: getProductListSummary(productList),
+		promotion,
 		totalPrice: totalPrice.toFixed(2),
 		discountValue: discountValue.toFixed(2),
-		discount: discount.toFixed(2) + '%'
+		discount: `${discount.toFixed(2)}%`
 	}
-	
+
 	return cart;
 }
 
 module.exports = {
 	getShoppingCart
 } 
-
